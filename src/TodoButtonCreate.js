235d31edf89671b86a4e941f34d2d47005bbb787
vagrant <vagrant@homestead>
2023-06-16 02:20:40 +0000

    progress class6

--- src/TodoButtonCreate.js
+++ src/TodoButtonCreate.js
@@ -2,7 +2,13 @@ import './css/TodoButtonCreate.css'
 
 function TodoButtonCreate() {
   return (
-    <button className="CreateTodoButton"> + </button>
+    <button className="CreateTodoButton"
+    onClick={(event) => {
+      console.log('hola le diste click');
+      console.log(event);
+      console.log(event.target);
+    }}
+    > + </button>
   )
 }
 export {TodoButtonCreate}
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,13 @@ import './css/TodoSearch.css'
 
 function TodoSearch() {
   return (
-    <input placeholder="Search Todo Items." className='TodoSearch' />
+    <input placeholder="Search Todo Items." className='TodoSearch'
+    onChange={(event)=>{
+      console.log('escribiste algo');
+      console.log(event);
+      console.log(event.target.value);
+    }}
+    />
   )
 }
 
