6dd50e8c3a4d5eca5b0aec1ba7a2de6cb4e297f3
Renier Vargas Homestead <renier1989@gmail.com>
2023-06-16 17:28:38 +0000

    progress class14

--- src/App.js
+++ src/App.js
@@ -18,34 +18,53 @@ import React from "react";
 
 // localStorage.setItem('TODOS_V1',parsedTodos);
 
-function App() {
-  const todos_local = localStorage.getItem('TODOS_V1');  // creo una variable que se va a llenar con los datos del localStorage
-  let parsedTodos;   // creo un variable de inicio donde se van a guardar la lista de los TODOS
 
+
+// Esto es un custom hooks
+// esto va a recibir 2 parametros uno sera el itemName que asignara el nombre donde se almacenar las cosas en el localStorage ej: 'TODOS_V1'
+// tambien va a recibir un valor inicial que sera el initialValue , esto va a ser cualquier tipo de variable a ser almecenado en el localStorage
+function useLocalStorage(itemName ,initialValue){
+
+  const todos_local = localStorage.getItem(itemName);  // creo una variable que se va a llenar con los datos del localStorage
+  let parsedItem;   // creo un variable de inicio donde se van a guardar la lista de los TODOS
+  
   // aqui valido si es que existen datos en el localStorage sino es asi entonces creo la variable en el local o cargo la lista
   if(!todos_local){
-    localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    parsedTodos = [];
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
   }else{
-    parsedTodos = JSON.parse(todos_local);
+    parsedItem = JSON.parse(todos_local);
   }
+
+  // creo un estado donde pueda manejar lo que esta en el localstorage
+  const [item, setItem] = React.useState(parsedItem);
+
+  // aqui guardo los datos de la lista tanto en el localStorage como el estado de react
+  const saveItem = (newItem) => {
+    localStorage.setItem(itemName,JSON.stringify(newItem));
+    setItem(newItem);
+  }
+
+  // aqui exporto las cosas que necesito utilizar del lado de la App 
+  // el saveItem es el que se va a encargar de actualizar el localStorage y el estado que posee la lista de los TODOS
+  // el item posee la lista en si 
+  // para usarlos del lado del App debo tener el cuenta el orden como los estoy importando 
+  return [item, saveItem];
+
+}
+
+function App() {
+  //aqui hago uso de mi custom Hook , debo tener el cuenta el orden como exporte los elemetos desdel el custom hook para llamarlos
+  // estos puedes tener cualquier nombre pero siempre respetando el orden de exportacion 
+  // por ejemplo : en la exportacion desde el hook es "item" aqui lo llamo "todos" igual para "saveItem" -> "saveTodos"
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1' , []); 
   
   const [searchValue, setSearchValue] = React.useState('');
-  // console.log(searchValue);
-
-  const [todos, setTodos] = React.useState(parsedTodos); //Aqui cargo la lista de los TODOS  que vienen del localstorage
-  // const [todos, setTodos] = React.useState(defaultTodos);
   const completedTodos = todos.filter((todo)=> !!todo.completed === true).length;
   const totalTodos = todos.length;
   const searchedTodos = todos.filter((todo)=>(
     todo.text.toLowerCase().includes(searchValue.toLowerCase())
   ));
-
-  // aqui guardo los datos de la lista tanto en el localStorage como el estado de react
-  const saveTodos = (newTodos) => {
-    localStorage.setItem('TODOS_V1',JSON.stringify(newTodos));
-    setTodos(newTodos);
-  }
 
   // esta funcion es para cambiar el status del item
   const chageStatusItem = (text) => {
