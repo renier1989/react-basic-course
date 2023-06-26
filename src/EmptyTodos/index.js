0f9ab8917dc1c7d8d83b86cdea8326b7db2575b1
Renier Vargas Homestead <renier1989@gmail.com>
2023-06-26 15:33:31 +0000

    some little changes on Styles

--- src/EmptyTodos/index.js
+++ src/EmptyTodos/index.js
@@ -3,7 +3,25 @@ import '../css/EmptyTodos.css'
 
 function EmptyTodos() {
   return (
-    <div>EmptyTodos</div>
+    <div>
+      
+      <li className="TodoItem-empty">
+        {/* <span className={`Icon Icon-check ${todo.completed && "Icon-check--active"}`}
+        onClick={onComplete}
+        > V </span> */}
+
+        {/* <CompleteIcon completed={todo.completed} onComplete={onComplete} /> */}
+        
+        <p className='TodoItem-p-empty'>Sorry we couldn't find TODO Items to show!</p>
+
+        {/* <DeleteIcon onDelete={onDeleteItem}/> */}
+
+
+        {/* <span className='Icon Icon-delete'
+        onClick={onDeleteItem}
+        > X </span> */}
+      </li>
+    </div>
   )
 }
 
