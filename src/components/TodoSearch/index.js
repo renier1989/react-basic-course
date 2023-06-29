c954be34740d5502f65fe28948bf71c3350e39ab
Renier Vargas <renier1989@gmail.com>
2023-06-29 22:08:18 +0000

    using useSearchParams, to search todos with the values in the url.

--- src/components/TodoSearch/index.js
+++ src/components/TodoSearch/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import '../../css/TodoSearch.css'
 import { TodoContext } from '../TodoContext';
+import { useNavigate } from 'react-router';
+import { useSearchParams } from 'react-router-dom';
 
 function TodoSearch() {
   const {
     searchValue, setSearchValue
   } = React.useContext(TodoContext);
+
+
+  // uso esto para poder implementar la busqueda en el navegador 
+  const navigate = useNavigate();
+  const [searchParmas, setSerachParamas] = useSearchParams();
+
+  const initialSearchValue = searchParmas.get('search') || '';
+
+  React.useEffect(()=>{
+    setSearchValue(initialSearchValue);
+  },[initialSearchValue, setSearchValue]);
+
+
   return (
     <input placeholder="Search Todo Items." className='TodoSearch'
-    value={searchValue}
+    value={searchValue || initialSearchValue}
     onChange={(event)=>{
       setSearchValue(event.target.value);
+      navigate(`?search=${event.target.value}`);
     }}
     />
   )
